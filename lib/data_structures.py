264db33468ab15c2308f364f70914e9049540881
michaelanbrown <m_brownbrown@yahoo.com>
2024-08-22 12:40:03 -0400

    starting deliverable 6 by getting the sum

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -36,7 +36,7 @@ def print_spiciest_foods(spicy_foods):
     print_spicy_foods(spiciest_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    return sum([food["heat_level"] for food in spicy_foods])
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
